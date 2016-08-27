b9e1af3efe2c6927bb0071194323fd5b1bc8a0b3
user <user@users-MBP.fios-router.home>
2016-08-27 01:29:04 -0400

    add win lose logic and sounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,74 +3,149 @@
  * Created by Anthony Delgado on 8/25/16.
  */
 
+$(document).ready(function () {
 
-var winCount = 0;
-var gameOver = true;
-var playerCharacter = null;
-var playerOpponent = null;
-var players = [ 'Ryu', 'Chun', 'Ken', 'Cammy', 'Blanka', 'Bison' ];
+    var winCount = 0;
+    var loseCount = 0;
+    var gameOver = true;
+    var playerCharacter = null;
+    var playerCharacterHealth = 100;
+    var playerOpponent = null;
+    var playerOpponentHealth = 100;
+    var players = ['Ryu', 'Chun', 'Ken', 'Cammy', 'Blanka', 'Bison'];
 
-$( document ).ready(function() {
 
-    console.log( "game is ready!" );
+    console.log("game is ready!");
 
-function startGame() {
+    // Adds a source element, and appends it to the audio element, represented
+    // by elem.
+    function addSource(elem, path) {
+        $('<source />').attr('src', path).appendTo(elem);
+    }
 
-    // When the game starts, the player will choose a character by clicking on the fighter's picture. The player will fight as that character for the rest of the game.
-
-    console.log( "loading players!" );
-
-    for(var i = 0; i < players.length; i++ ) {
-        console.log(players[i]);
-        var player = $('<div>');
-        player.addClass('col-sm-2 playerSelect');
-        player.attr('data-player', players[i]);
-        player.attr('id', players[i]);
-        player.text(players[i]);
-
-        //create playerAvatar
-        var playerAvatar = $('<img>');
-        playerAvatar.addClass('img-responsive');
-        playerAvatar.attr('data-player', players[i]);
-        playerAvatar.attr('src', 'assets/images/players/' + players[i] + '.png');
-        playerAvatar.text(players[i]);
-
-        //add playerAvatar to the player container
-        $(playerAvatar).appendTo(player);
-        // print players to the div with ID arena
+    function soundEffect(filename, filetype = 'wav') {
+      // use jQuery to insert an HTML5 audio element into the DOM
+    // <audio autoplay>
+    // <source src="horse.ogg" type="audio/wav">
+    // </audio>
+        var player = $('<audio />', {
+            autoPlay : 'autoplay'
+        });
+        addSource(player, window.location.host + '/streetfighterjs/assets/sounds/' + filename + '.' + filetype);
         $(player).appendTo("#arena");
     }
-    gameOver = false;
 
-    $('.playerSelect').on('click', function() {
-        /*
-         When you click on a playerSelect div
-         */
-        if((playerCharacter === null ) ){
-            playerCharacter = $(this).data('player');
-            console.log('You are now playing as ' + playerCharacter);
-            $(this).addClass('col-sm-6');
-            $(this).removeClass('col-sm-2 playerSelect');
-        }else if ((playerOpponent === null)){
-            // The player chooses an opponent by clicking on an enemy's picture.
-            playerOpponent = $(this).data('player');
-            console.log('You are now playing against ' + playerOpponent);
-            $(this).addClass('col-sm-6');
-            $(this).removeClass('col-sm-2 playerSelect');
+    function startGame() {
+
+        // When the game starts, the player will choose a character by clicking on the fighter's picture. The player will fight as that character for the rest of the game.
+
+        console.log("loading players!");
+
+        for (var i = 0; i < players.length; i++) {
+            console.log(players[i]);
+            var player = $('<div>');
+            player.addClass('col-sm-4 playerSelect');
+            player.attr('data-player', players[i]);
+            player.attr('id', players[i]);
+            player.text(players[i]);
+
+            //create playerAvatar
+            var playerAvatar = $('<img>');
+            playerAvatar.addClass('img-responsive');
+            playerAvatar.attr('data-player', players[i]);
+            playerAvatar.attr('src', 'assets/images/players/' + players[i] + '.png');
+            playerAvatar.text(players[i]);
+
+            //add playerAvatar to the player container
+            $(playerAvatar).appendTo(player);
+            // print players to the div with ID arena
+            $(player).appendTo("#arena");
         }
-    });
+        gameOver = false;
 
-}
+        $('.playerSelect').on('click', function () {
+            /*
+             When you click on a playerSelect div
+             */
 
+            // the first select is your character
+            if ((playerCharacter === null )) {
+                playerCharacter = $(this).data('player');
+                $('#playerCharacterText').text(playerCharacter);
+                console.log('You are now playing as ' + playerCharacter);
+                $(this).addClass('col-sm-6');
+                $(this).removeClass('col-sm-2 playerSelect');
+            } else if ((playerOpponent === null)) {
+                // the next selection is your opponent
+                // The player chooses an opponent by clicking on an enemy's picture.
+                playerOpponent = $(this).data('player');
+                $('#playerOpponentText').text(playerOpponent);
+                console.log('You are now playing against ' + playerOpponent);
+                $(this).addClass('col-sm-6');
+                $(this).removeClass('col-sm-2 playerSelect');
+                $('.playerSelect').fadeOut();
+                soundEffect('fight');
+            }
+        });
+
+    }
+
+    function startAttack() {
+
+        var damageOpponent = Math.floor((Math.random() * 20) + 1);
+
+        playerOpponentHealth = playerOpponentHealth - damageOpponent;
+
+        console.log(playerOpponent + 's health is now ' + playerOpponentHealth + ' reduced by ' + damageOpponent);
+
+        $("#playerOpponentHealthBar").css('width', playerOpponentHealth + "%");
+
+        // if your Opponents Health is less than 1 after this attack you win!
+        if (playerOpponentHealth < 1) {
+            swal('You Win!');
+            winCount++;
+            gameOver = true;
+        } else {
+
+            // if the Opponent is still alive damage the other player after 1 second
+            setTimeout(function () {
+
+
+                var damageCharacter = Math.floor((Math.random() * 20) + 1);
+
+                playerCharacterHealth = playerCharacterHealth - damageCharacter;
+
+                console.log(playerCharacter + 's health is now ' + playerCharacterHealth + ' reduced by ' + damageCharacter);
+
+                $("#playerCharacterHealthBar").css('width', playerCharacterHealth + "%");
+                if (playerCharacterHealth < 1) {
+                    swal('You lose!');
+                    loseCount++;
+                    gameOver = true;
+                }
+            }, 1000);
+
+        }
+
+
+    }
+
+    // #playerOpponentHealthBar
 
 // Click functions below
 
-$( "#startGame" ).click(function() {
-    // alert( "Handler for .click() called." );
-    startGame();
-});
+    $("#startGame").click(function () {
+        //  gameOver = false;
+        if (gameOver === true) {
+            startGame();
+        }
+    });
+    // fire when user clicks Attack button
 
-
+    $("#startAttack").click(function () {
+        //  gameOver = false;
+        startAttack();
+    });
 
 
 });
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 8/27/16
+ * Time: 1:26 AM
+ */
+
+include('index.html');
